80ccc1000c0febccc1279dd312859ef67ff29439
Andrew Sanchez <andrewfsanchez@gmail.com>
2020-03-10 20:43:35 -0400

    passwords are now hashed with salt and pepper before being stored

--- app.py
+++ app.py
@@ -14,22 +14,58 @@ def register():
         email = request.form["email"]
         username = request.form["username"]
         funds = request.form["funds"]
-        password = request.form["password"]
-
+        password = bytes(request.form["password"], 'utf-8')
+        pepper = bytes("sneezeSauce", 'utf-8')
         salt = bcrypt.gensalt()
-        encryptedPass= bcrypt.hashpw(password.encode('utf8'), salt)
+        encryptedPass= bcrypt.hashpw(password + pepper, salt)
 
         try:
             querry="""INSERT INTO accounts 
                     (email, username, funds, password)
-                    Values (?,?,?,?);"""
-            cursor.execute(querry, (email,username,funds,password))
+                    VALUES (?,?,?,?)"""
+            cursor.execute(querry, (email,username,funds,encryptedPass))
             connection.commit()
-            return redirect('/')
+            connection.close()
+            return redirect('/login')
         except:
             "Registration Error"
+            print("error")
+            return redirect('/error')
     else:
         return render_template("register.html")
+
+@app.route('/login', methods= ['POST', 'GET'])
+def login():
+
+    connection = sqlite3.connect('fakebank.db')
+    cursor= connection.cursor()
+
+    if (request.method == 'POST'):
+        email= request.form["email"]
+        password=bytes(request.form["password"], 'utf-8')
+        try:
+            
+            cursor.execute("SELECT * FROM accounts WHERE email= ?", (email,))
+           
+            connection.commit()
+            
+            hashedpass= cursor.fetchone()[3]
+            pepper = bytes("sneezeSauce", 'utf-8')
+            if bcrypt.checkpw(password + pepper, hashedpass):
+                #login token stuff
+                return redirect('/')
+            else:
+                #err
+                return redirect('/login')
+
+        except:
+            "querry error"
+            print('error')
+
+
+    else:
+        return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- createDB.py
+++ createDB.py
@@ -3,7 +3,6 @@ import sqlite3
 connection = sqlite3.connect('fakebank.db')
 cursor= connection.cursor()
 
-
-cursor.execute("CREATE TABLE IF NOT EXISTS accounts (email TEXT, username TEXT, funds INTEGER, password STRING, PRIMARY KEY(email))")
+cursor.execute("CREATE TABLE IF NOT EXISTS accounts (email TEXT, username TEXT, funds INTEGER, password TEXT, PRIMARY KEY(email))")
 connection.commit()
 connection.close()
